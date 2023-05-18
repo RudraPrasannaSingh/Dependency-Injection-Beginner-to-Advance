d767a597d950dc0ad0c582b52b1de93f3f3db13d
Rudra Singh <rudra.singh@mitratech.com>
2023-05-18 16:08:25 +0530

    Intermediate level

---
+++ Dependeny_Injection_Beginner_Advance/IDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependeny_Injection_Beginner_Advance
+{
+   public interface IDataProvider
+   {
+      string GetData();
+   }
+
+   public class DatabaseDataProvider : IDataProvider
+   {
+      private readonly string _connectionString;
+
+      public DatabaseDataProvider(string connectionString)
+      {
+         _connectionString = connectionString;
+      }
+
+      public string GetData()
+      {
+         // Retrieve data from the database using the connection string
+         return "Data from the database";
+      }
+   }
+
+}
--- Dependeny_Injection_Beginner_Advance/Program.cs
+++ Dependeny_Injection_Beginner_Advance/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Dependeny_Injection_Beginner_Advance
 {
@@ -10,10 +12,26 @@ namespace Dependeny_Injection_Beginner_Advance
    {
       static void Main(string[] args)
       {
-         var logger = new Logger();  // Create an instance of Logger
-         var myClass = new MyClass(logger);  // Inject Logger into MyClass
+         //Beginner level
 
-         myClass.DoSomething();  // Use MyClass with injected Logger
+         //var logger = new Logger();  // Create an instance of Logger
+         //var myClass = new MyClass(logger);  // Inject Logger into MyClass
+
+         //myClass.DoSomething();  // Use MyClass with injected Logger
+
+         //Intermediate Level
+
+         var serviceProvider = new ServiceCollection()
+            .AddSingleton<Logger>()  // Register Logger as a singleton
+            .AddTransient<MyClass>()  // Register MyClass with transient lifetime
+            .BuildServiceProvider();
+
+         var myClass = serviceProvider.GetRequiredService<MyClass>();
+
+         myClass.DoSomething();
+         Console.ReadLine();
+
+
 
       }
    }
