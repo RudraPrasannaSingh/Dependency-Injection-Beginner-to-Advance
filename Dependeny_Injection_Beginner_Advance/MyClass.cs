c46e4aeeba114e67c48bcf0b0070f393a1474aad
Rudra Singh <rudra.singh@mitratech.com>
2023-05-18 14:39:49 +0530

    Second commit

---
+++ Dependeny_Injection_Beginner_Advance/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependeny_Injection_Beginner_Advance
+{
+   class MyClass
+   {
+      private readonly Logger _logger;
+
+      public MyClass(Logger logger)
+      {
+         _logger = logger;
+      }
+
+      public void DoSomething()
+      {
+         _logger.Log("Doing something...");
+      }
+   }
+}
---
+++ Dependeny_Injection_Beginner_Advance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependeny_Injection_Beginner_Advance
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var logger = new Logger();  // Create an instance of Logger
+         var myClass = new MyClass(logger);  // Inject Logger into MyClass
+
+         myClass.DoSomething();  // Use MyClass with injected Logger
+
+      }
+   }
+}
